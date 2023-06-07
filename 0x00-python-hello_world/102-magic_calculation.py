722e7c7afab516e521de85194a3218a214f55459
EG-AKANDE <emmanuel_akande@yahoo.com>
2023-06-06 17:15:41 -0700

    python function

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,5 +1,5 @@
+#!/usr/bin/python3
 def magic_calculation(a, b):
-
-	# This function takes in two arguments a and b.#
-	# It returns the result of (a ** b) + 98.#
+	# accepts two args a and b #
+	# returns (a ** b) + 98 #
 	return (a ** b) + 98
