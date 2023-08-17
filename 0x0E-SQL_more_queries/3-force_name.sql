6096d204c18e5c62dea082ed84ba4ced4a816149
Emmanuel Akande <118781468+EG-AKANDE@users.noreply.github.com>
2023-08-17 13:45:10 +0100

    Directory Delete

--- 0x0E-SQL_more_queries/3-force_name.sql
+++
@@ -1,5 +0,0 @@
--- creates the table force_name on mysql server
--- database name passed as an argument of the mysql command
-CREATE TABLE IF NOT EXISTS force_name (id INT,
-		name VARCHAR(256) NOT NULL
-		);
--- 0x0E-SQL_more_queries/5-unique_id.sql
+++
@@ -1,5 +0,0 @@
--- creates the table unique_id on mysql server
--- database name passed as an argument of the mysql command
-CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE,
-		name VARCHAR(256)
-		);
